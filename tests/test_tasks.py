0987131111e2eb63deb48390f02519ce049e7896
Laura Hollister <52159918+lhollister@users.noreply.github.com>
2023-09-10 16:52:21 +0200

    Update test_tasks.py

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,189 +1,164 @@
-from fixtures import *
-from inspect import signature
-from types import FunctionType
-from unittest.mock import MagicMock
-import matplotlib as mpl
-from matplotlib.testing.decorators import check_figures_equal
+"""Test Tasks
+Laura Hollister
+Septemvber 2023
+    """
+
 import pytest
-from utils import check_figures_equal
-
 import numpy as np
-import pylab as pl
+import matplotlib.pyplot as plt
 from types import FunctionType, MethodType
-from conftest import *
 
 
-class TestTask1:
-    # TODO: how to check time_to_collision without defining balls?
+class TestTask1():
+    def test_pos_method(self, ball_mod):
+        assert isinstance(ball_mod.Ball.pos, FunctionType)
 
-    # --- functionality checks
+    def test_default_pos_array(self, ball_mod):
+        ball = ball_mod.Ball()
+        assert isinstance(ball.pos(), np.ndarray)
 
-    def test_correct_dt_1(self, ball_module):
-        """
-        check correct dt is calculated
-        (balls moving towards each other on x-axis)
-        """
+    def test_default_ball_pos(self, default_ball):
+        assert np.allclose(default_ball.pos(), np.array([0, 0]))
 
-        # TODO TEST INCORRECT - balls moving away from each other
+    def test_set_ball_pos(self, ball_mod):
+        ball = ball_mod.Ball(pos=[1., 2.])
+        assert np.allclose(ball.pos(), np.array([1., 2.]))
 
-        ball1 = ball_module.Ball(mass=0.1, radius=0.1,
-                                 pos=[-0.1, 0.], vel=[-1., 0.])
-        ball2 = ball_module.Ball(mass=0.1, radius=0.1, pos=[
-                                 0.1, 0.], vel=[1.0, 0.])
-        dt = ball1.time_to_collision(ball2)
+    def test_pos_input(self, ball_mod):
+        with pytest.raises(Exception):
+            ball = ball_mod.Ball(pos=[1, 2, 3])
+        with pytest.raises(Exception):
+            ball = ball_mod.Ball(pos=[1])
+        with pytest.raises(Exception):
+            ball = ball_mod.Ball(pos=4)
 
-        assert np.isclose(
-            dt, 0.2), "Colliision expected, with dt=0.2. Please check time_to_collision()."
+    def test_vel_method(self, ball_mod):
+        assert isinstance(ball_mod.Ball.vel, FunctionType)
 
-    def test_correct_dt_2(self, ball_module):
-        """
-        check correct dt is calculated - no collision
-        (balls moving away from each other on x-axis)
-        TODO BALLS ALREADY TOUCHING
-        """
+    def test_default_vel_array(self, default_ball):
+        assert isinstance(default_ball.vel(), np.ndarray)
 
-        ball1 = ball_module.Ball(mass=0.1, radius=0.1,
-                                 pos=[-0.11, 0.], vel=[-1., 0.])
-        ball2 = ball_module.Ball(mass=0.1, radius=0.1, pos=[
-                                 0.11, 0.], vel=[1.0, 0.])
-        dt = ball1.time_to_collision(ball2)
+    def test_default_ball_vel(self, default_ball):
+        assert np.allclose(default_ball.vel(), np.array([1, 0]))
 
-        assert dt is None, "No collision expected, dt should return None. Please check time_to_collision()."
+    def test_set_ball_vel(self, ball_mod):
+        ball = ball_mod.Ball(vel=[1., 2.])
+        assert np.allclose(ball.vel(), np.array([1., 2.]))
 
-    def test_correct_dt_3(self, ball_module):
-        """
-        check correct dt is calculated - opposite collision
-        (balls moving towards each other)
-        GOOD TEST
-        """
+    def test_vel_input(self, ball_mod):
+        with pytest.raises(Exception):
+            ball = ball_mod.Ball(vel=[1, 2, 3])
+        with pytest.raises(Exception):
+            ball = ball_mod.Ball(vel=[1])
+        with pytest.raises(Exception):
+            ball = ball_mod.Ball(vel=4)
 
-        ball1 = ball_module.Ball(mass=0.1, radius=0.1, pos=[
-                                 1.0, 0.], vel=[1., 3.])
-        ball2 = ball_module.Ball(mass=0.1, radius=0.1, pos=[
-                                 4.0, 1.], vel=[-2.0, 2.0])
-        dt = ball1.time_to_collision(ball2)
+    def test_mass_method(self, ball_mod):
+        assert isinstance(ball_mod.Ball.mass, FunctionType)
 
-        assert np.isclose(
-            dt, 0.936754446), "Collision expected, with dt~0.936754446. Please check time_to_collision()."
+    def test_mass_type(self, default_ball):
+        assert isinstance(default_ball.mass(), float)
 
-    def test_correct_dt_4(self, ball_module):
-        """
-        check correct dt is calculated - correct collision
-        (balls moving away from each other)
-        GOOD TEST
-        """
+    def test_default_mass(self, default_ball):
+        assert default_ball.mass() == 1.
 
-        ball1 = ball_module.Ball(mass=0.1, radius=0.1, pos=[
-                                 1.0, 0.], vel=[1., 3.])
-        ball2 = ball_module.Ball(mass=0.1, radius=0.1, pos=[
-                                 4.0, 1.], vel=[2.0, 2.0])
-        dt = ball1.time_to_collision(ball2)
+    def test_set_mass(self, ball_mod):
+        ball = ball_mod.Ball(mass=5)
+        assert ball.mass() == 5.
 
-        assert dt is None, "no collision expected, dt should return None. Please check time_to_collision()."
+    def test_radius_method(self, ball_mod):
+        assert isinstance(ball_mod.Ball.radius, FunctionType)
 
+    def test_radius_type(self, default_ball):
+        assert isinstance(default_ball.radius(), float)
 
+    def test_default_radius(self, default_ball):
+        assert default_ball.radius() == 0.1
+
+    def test_set_radius(self, ball_mod):
+        ball = ball_mod.Ball(radius=2)
+        assert ball.radius() == 2.
+
+    def test_move_exists(self, ball_mod):
+        assert hasattr(ball_mod.Ball, 'move')
+
+    def test_move_correct(self, default_ball):
+        default_ball.move(3)
+        assert np.allclose(default_ball.pos(), np.array([3, 0]))
+
+    def move_correct(self, default_ball):
+        with pytest.raises(Exception):
+            default_ball.move(-0.4)
 
 class TestTask2():
-    def test_mass_exists(self, my_ball):
-        assert hasattr(my_ball, '_Ball__mass')
+    def test_default_con_rad_type(self, default_con):
+        assert isinstance(default_con.radius, MethodType)
+    
+    def test_default_rad_type(self, default_con):
+        assert isinstance(default_con.radius(), float)
+    
+    def test_default_con_rad(self, default_con):
+        assert default_con.radius() == 10.
+        
+    def test_repr(self, con_class):
+        assert hasattr(con_class, '__repr__')
 
-    def test_radius_exists(self, my_ball):
-        assert hasattr(my_ball, '_Ball__radius')
+class TestTask3():
+    def test_repr(self, ball_mod):
+        assert hasattr(ball_mod.Ball, '__repr__')
 
-    def test_pos_exists(self, my_ball):
-        assert hasattr(my_ball, '_Ball__pos')
+    def test_get_patch_ball(self, ball_mod, default_ball):
+        assert isinstance(ball_mod.Ball.get_patch, FunctionType)
+        assert isinstance(default_ball.get_patch(), plt.Circle)
 
-    def test_pos_type(self, my_ball):
-        assert isinstance(my_ball.pos(), np.ndarray)
+    def test_get_patch_container(self, ball_mod, default_con):
+        assert isinstance(ball_mod.Container.get_patch, FunctionType)
+        assert isinstance(default_con.get_patch(), plt.Circle)
+        
+        
+class TestTask4():
+    def test_time_to_collision_parallel(self, ball_mod):
+        ball1 = ball_mod.Ball(pos=[0.1, 0], vel=[0., 2.])
+        ball2 = ball_mod.Ball(pos=[0., 1.], vel=[0., 2.])
+        assert ball1.time_to_collision(ball2) is None
+    
+    def test_time_to_collision_past(self, ball_mod):
+        ball1 = ball_mod.Ball(pos=[2, 0], vel=[1, 0])
+        ball2 = ball_mod.Ball(pos=[-3, 0], vel=[-1, 0])
+        assert ball1.time_to_collision(ball2) is None   
+    
+    
+    def test_time_to_collision_balls(self, ball_mod):
+        ball1 = ball_mod.Ball(pos=[1, 1], vel=[1., 0])
+        ball2 = ball_mod.Ball(pos=[5., 4.], vel=[0, -0.75])
+        assert np.isclose(ball1.time_to_collision(ball2), 3.84)
+               
+    def test_time_to_collision_con(self, ball_mod, default_con):
+        ball1 = ball_mod.Ball(pos=[1., -1.], vel=[0., 2.])
+        assert np.isclose(ball1.time_to_collision(default_con), 5.42468273)
 
-    def test_vel_exists(self, my_ball):
-        assert hasattr(my_ball, '_Ball__vel')
+class TestTask5():
+    def test_collide_functionality_1(self, ball_mod):
+        ball1 = ball_mod.Ball(pos=[1, 1.], vel=[1, 0])
+        ball2 = ball_mod.Ball(pos=[5, 4], vel=[0., -0.75])
+        time = ball1.time_to_collision(ball2)
+        ball1.move(time)
+        ball2.move(time)
+        ball1.collide(ball2)
 
-    def test_vel_type(self, my_ball):
-        assert isinstance(my_ball.vel(), np.ndarray)
+        assert np.allclose(ball1.pos(),[4.84, 1])
+        assert np.allclose(ball2.pos() ,[5, 1.12])
+        assert np.allclose(ball1.vel(),[0, -0.75])
+        assert np.allclose(ball2.vel(),[1,0])
 
-    def test_patch_exists(self, my_ball):
-        assert hasattr(my_ball, '_Ball__patch')
+    def test_collide_functionality_2(self, ball_mod, default_con):
+        ball1 = ball_mod.Ball(pos=[1., -1.], vel=[0.,2.])
+        time = ball1.time_to_collision(default_con)
+        ball1.move(time)
+        ball1.collide(default_con)
 
-    # --- has method checks
-
-    def test_pos_method(self, my_ball):
-        assert isinstance(my_ball.pos, MethodType)
-
-    def test_vel_method(self, my_ball):
-        assert isinstance(my_ball.vel, MethodType)
-
-    def test_move_method(self, my_ball):
-        assert isinstance(my_ball.move, MethodType)
-
-    def test_time_to_collision_method(self, my_ball):
-        assert isinstance(my_ball.time_to_collision, MethodType)
-
-    def test_collide_method(self, my_ball):
-        assert isinstance(my_ball.collide, MethodType)
-
-    def test_get_patch_method(self, my_ball):
-        assert isinstance(my_ball.get_patch, MethodType)
-
-
-
-class TestTask3:
-
-    # --- has attribute checks
-
-    def test_balls_exists(self, my_simulation, return_object_var):
-        balls = return_object_var(my_simulation, "balls")
-        if balls is None:
-            balls = return_object_var(my_simulation, "Balls")
-        assert balls is not None, "balls attribute does not exist in Simulation class"
-
-    def test_container_exists(self, my_simulation, return_object_var):
-        container = return_object_var(my_simulation, "container")
-        if container is None:
-            container = return_object_var(my_simulation, "Container")
-        assert container is not None, "container attribute does not exist in Simulation class"
-
-    # --- has method checks
-
-    def test_initialise_exists(self, my_simulation, return_object_method):
-        assert return_object_method(
-            my_ball, "initialise") is not None, "initialise() method does not exist in Simulation class"
-
-    def test_next_collision_exists(self, my_simulation, return_object_method):
-        assert return_object_method(
-            my_ball, "next_collision") is not None, "next_collision() method does not exist in Simulation class"
-
-    def test_run_exists(self, my_simulation, return_object_method):
-        assert return_object_method(
-            my_ball, "run") is not None, "run() method does not exist in Simulation class"
-
-
-class TestTask4:
-
-    # --- functionality checks
-
-    def test_correct_dt(self, ball_module, my_container):
-        """
-        check correct dt is calculated - correct collision
-        (ball collides with container)
-        """
-
-        ball = ball_module.Ball(mass=0.1, radius=0.1, pos=[
-                                9.8, 0.1], vel=[-1.0, 0.])
-        dt = ball.time_to_collision(my_container)
-
-        assert np.isclose(
-            dt, 19.6994949), "Collision of ball with container expected, please check time_to_collision()"
-
-    def test_collide(self, ball_module, my_container):
-        """
-        check ball and container velocity after collision
-        """
-
-        ball = ball_module.Ball(mass=0.1, radius=0.1, pos=[
-                                9.8, 0.1], vel=[-1.0, 0.])
-        dt = ball.time_to_collision(my_container)
-        ball.move(dt)
-        ball.collide(container)
-
+        assert np.allclose(ball1.pos() ,[1., 9.84936546])
+        assert np.allclose(default_con.pos(),[0., 0.])
+        assert np.allclose(ball1.vel(),[-0.4019739 , -1.95918784])
+        assert np.allclose(default_con.vel() ,[0., 0.])
 
