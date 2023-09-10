34e7af8ade3df10e05b13827f8f17b47806ca5a1
Laura Hollister <52159918+lhollister@users.noreply.github.com>
2023-09-10 16:51:08 +0200

    Fixtures changed to thermo fixtures

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -21,72 +21,31 @@ class CombinedNamespace:
             yield from vars(module).keys()
 
 @pytest.fixture
-def rt():
-    rays_module = import_module("raytracer.rays")
-    elements_module = import_module("raytracer.elements")
-    lenses_module = import_module("raytracer.lenses")
-    return CombinedNamespace(rays_module, elements_module, lenses_module)
+def ball_mod():
+    return import_module('ball')
+
 
 @pytest.fixture
-def ph():
-    return import_module("raytracer.physics")
+def default_ball(ball_mod):
+    return ball_mod.Ball()
 
 @pytest.fixture
-def rays():
-    return import_module("raytracer.rays")
+def con_class(ball_mod):
+    return ball_mod.Container
 
 @pytest.fixture
-def elements():
-    return import_module("raytracer.elements")
+def default_con(ball_mod):
+    return ball_mod.Container()
+
 
 @pytest.fixture
-def lenses():
-    return import_module("raytracer.lenses")
+def sim_module():
+    return import_module("simulation")
 
 @pytest.fixture
-def an():
-    yield import_module("raytracer.analysis")
-    plt.close()
-
-@pytest.fixture
-def var_name_map(rays):
-    pos = [1.,2.,3.]
-    direc = [4., 5., 6.]
-    udirec = np.array(direc) / np.linalg.norm(direc)
-    r = rays.Ray(pos=pos, direc=direc)
-
-    ret={}
-    for var_name, val in vars(r).items():
-        if np.allclose(val, pos):
-            ret['pos'] = var_name
-        elif np.allclose(val, direc) or np.allclose(val, udirec):
-            ret['direc'] = var_name
-    return ret
-
-@pytest.fixture
-def default_ray(rays):
-    return rays.Ray()
-
-@pytest.fixture
-def default_ray_pos(rays, var_name_map):
-    return getattr(rays.Ray(), var_name_map["pos"])
-
-@pytest.fixture
-def default_ray_direc(rays, var_name_map):
-    return getattr(rays.Ray(), var_name_map["direc"])
-
-@pytest.fixture
-def test_ray(rays):
-    return rays.Ray(pos=[1.,2.,3.], direc=[4.,5.,6.])
-
-@pytest.fixture
-def test_ray_pos(rays, var_name_map):
-    return getattr(rays.Ray(pos=[1.,2.,3.], direc=[4.,5.,6.]), var_name_map["pos"])
-
-@pytest.fixture
-def test_ray_direc(rays, var_name_map):
-    return getattr(rays.Ray(pos=[1.,2.,3.], direc=[4.,5.,6.]), var_name_map["direc"])
-
-@pytest.fixture
-def ray_bundle(rays):
-    return getattr(rays, "RayBundle", getattr(rays, "Bundle", None))
+def my_simulation(simulation_module):
+    """
+    create simulation instance
+    """
+    my_simulation = simulation_module.Simulation()
+    return my_simulation
