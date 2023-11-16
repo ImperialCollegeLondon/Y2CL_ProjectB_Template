84e65bfe681c043a4ea3a809c196a657edd9faa2
Alexander Richards <a.richards@imperial.ac.uk>
2023-11-16 11:47:18 +0000

    Update test_code_quality.py

--- tests/test_code_quality.py
+++ tests/test_code_quality.py
@@ -33,7 +33,7 @@ class TestDocumentation:
     def test_documentation_present(self):
         project_path = os.path.split(os.path.dirname(__file__))[0]
         course_files = " ".join(repr(path) for path in glob(os.path.join(project_path, "thermosnooker", "[a-zA-Z]*.py")))
-        cmd = rf"pydocstyle --select=D100,D102,D103,D419 {course_files}"
+        cmd = rf'pydocstyle --select=D100,D102,D103,D419 "{course_files}"'
         res = run(cmd, shell=True, capture_output=True, check=False, text=True)
         if res.returncode:
             print("pydocstyle")
@@ -42,16 +42,17 @@ class TestDocumentation:
             print(f"return code: {res.returncode}")
             print(f"stdout:\n{res.stdout}")
             print(f"stderr:\n{res.stderr}")
-        assert res.returncode == 0, "pydocstyle failed"
+            assert not res.stderr, "Problem running pydocstyle command."
         missing_docs = len(res.stdout.splitlines()) / 2
         if missing_docs:
-            print(res.stdout)
+            print(f"Num of problems: {missing_docs}")
+            print(f"stdout:\n{res.stdout}")
         assert missing_docs == 0
 
     def test_documentation_style(self):
         project_path = os.path.split(os.path.dirname(__file__))[0]
         course_files = " ".join(repr(path) for path in glob(os.path.join(project_path, "thermosnooker", "[a-zA-Z]*.py")))
-        cmd = rf"darglint {course_files}"
+        cmd = rf'darglint "{course_files}"'
         res = run(cmd, shell=True, capture_output=True, check=False, text=True)
         if res.returncode:
             print("darglight")
@@ -60,8 +61,9 @@ class TestDocumentation:
             print(f"return code: {res.returncode}")
             print(f"stdout:\n{res.stdout}")
             print(f"stderr:\n{res.stderr}")
-        assert res.returncode == 0, "darglint failed"
-        malformed_docs = len(res.stdout.splitlines())
+            assert not res.stderr, "Problem running darglint command."
+        malformed_docs = max(len(res.stdout.splitlines()) - 1, 0)
         if malformed_docs:
-            print(malformed_docs)
+            print(f"Num of problems: {malformed_docs}")
+            print(f"stdout:\n{res.stdout}")
         assert malformed_docs == 0
